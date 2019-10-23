d46d9d547229b5cc7060baa0146cdab397e011fc
Wilde <gutopicciani@yahoo.com.br>
2019-10-22 21:39:07 -0300

    Colocando keycloak na brincadeira

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
 const express = require('express');
 const app = express();
 
-app.get('/', function(req, res) {
-  res.send('Essa é a rota pública');
+const session = require('express-session');
+const Keycloak = require('keycloak-connect');
+
+const memoryStore = new session.MemoryStore();
+app.use(session({
+  secret: 'some secret',
+  resave: false,
+  saveUninitialized: true,
+  store: memoryStore,
+}));
+
+const keycloak = new Keycloak({ store: memoryStore });
+
+app.use(keycloak.middleware());
+
+app.get('/', keycloak.checkSso(), function(req, res) {
+  if (req.kauth.grant) {
+    res.send(`Essa é a rota pública. Você é sempre bem vindo aqui ${req.kauth.grant.access_token.content.name}.`);
+    return;
+  }
+  res.send('Essa é a rota pública, e você não está logado.');
 });
 
-app.get('/logged', function(req, res) {
-  res.send('Essa rota só poderá ser acessada por usuários logados');
+app.get('/logged', keycloak.protect(), function(req, res) {
+  res.send(`Essa rota só poderá ser acessada por usuários logados. Bem vindo ${req.kauth.grant.access_token.content.name}.`);
 });
 
-app.get('/role-access', function(req, res) {
+app.get('/role-access', keycloak.protect('realm:ticaracatica'), function(req, res) {
   res.send('Essa rota só poderá ser acessada por usuários que tenham a role ticaracatica');
 });
 
